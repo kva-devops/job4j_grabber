df40167e6df48c54e3e280272fe5860dd3ebed3a
vladimirKutyavin <kva.devops@gmail.com>
2021-06-26 17:53:16 +0400

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,59 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime crated;
+
+    public Post(int id, String title, String link, String description, LocalDateTime crated) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.crated = crated;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCrated() {
+        return crated;
+    }
+
+    public void setCrated(LocalDateTime crated) {
+        this.crated = crated;
+    }
+}
