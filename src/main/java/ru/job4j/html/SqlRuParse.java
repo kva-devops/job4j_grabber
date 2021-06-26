735d71366cf5109a87c4532e35a3ec4cd7525750
vladimirKutyavin <kva.devops@gmail.com>
2021-06-26 17:16:03 +0400

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -12,12 +12,18 @@ import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 public class SqlRuParse {
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("http://www.sql.ru/forum/job-offers").get();
-        Element table = doc.getElementsByTag("tbody").get(2);
-        Elements ourTable = table.children();
-        List<Element> afterSkip = new ArrayList<>();
-        ourTable.stream().skip(4).forEach(afterSkip::add);
-        for (Element elem : afterSkip) {
+        String urlTemplate = "http://www.sql.ru/forum/job-offers/";
+        int pageLimit = 5;
+        for (int i = 1; i < pageLimit; i++) {
+            System.out.printf("===============Page %d===============", i);
+            System.out.println();
+            String urlMain = urlTemplate + i;
+            Document doc = Jsoup.connect(urlMain).get();
+            Element table = doc.getElementsByTag("tbody").get(2);
+            Elements ourTable = table.children();
+            List<Element> afterSkip = new ArrayList<>();
+            ourTable.stream().skip(4).forEach(afterSkip::add);
+            for (Element elem : afterSkip) {
                 Elements parseLink = elem.select(".postslisttopic");
                 for (Element td : parseLink) {
                     Element href = td.child(0);
@@ -26,6 +32,7 @@ public class SqlRuParse {
                 }
                 String publicTimeString = elem.children().get(5).select(".altCol").text();
                 System.out.println(new SqlRuDateTimeParser().parse(publicTimeString));
+            }
         }
     }
 }
