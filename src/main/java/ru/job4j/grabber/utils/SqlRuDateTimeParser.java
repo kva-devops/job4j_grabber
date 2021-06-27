3108267ec64fd8411ac695c098291d613777e635
vladimirKutyavin <kva.devops@gmail.com>
2021-06-27 10:12:33 +0400

    2.1. Преобразование даты [#289476 #171653] - correcting

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -3,25 +3,24 @@ package ru.job4j.grabber.utils;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
-import java.util.Set;
 
 import static java.util.Map.entry;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
 
     Map<String, String> calendarMap = Map.ofEntries(
-            entry("01", "янв"),
-            entry("02", "фев"),
-            entry("03", "мар"),
-            entry("04", "апр"),
-            entry("05", "май"),
-            entry("06", "июн"),
-            entry("07", "июл"),
-            entry("08", "авг"),
-            entry("09", "сен"),
-            entry("10", "окт"),
-            entry("11", "ноя"),
-            entry("12", "дек")
+            entry("янв", "01"),
+            entry("фев", "02"),
+            entry("мар", "03"),
+            entry("апр", "04"),
+            entry("май", "05"),
+            entry("июн", "06"),
+            entry("июл", "07"),
+            entry("авг", "08"),
+            entry("сен", "09"),
+            entry("окт", "10"),
+            entry("ноя", "11"),
+            entry("дек", "12")
     );
 
     public SqlRuDateTimeParser() {
@@ -31,35 +30,28 @@ public class SqlRuDateTimeParser implements DateTimeParser {
     public LocalDateTime parse(String parse) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         String[] buffDateTime = parse.split(" ");
+        String rsl;
+        LocalDateTime dateTime;
         if ("вчера,".equals(buffDateTime[0])) {
-            LocalDateTime dateTime = LocalDateTime.now().minusDays(1);
-            DateTimeFormatter formatterForYesterday = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-            String[] buffChangeTime = dateTime.format(formatterForYesterday).split(" ");
-            String rsl = buffChangeTime[0] + " " + buffDateTime[1];
-            return LocalDateTime.parse(rsl, formatter);
+            dateTime = LocalDateTime.now().minusDays(1);
         } else if ("сегодня,".equals(buffDateTime[0])) {
-            LocalDateTime dateTime = LocalDateTime.now();
-            DateTimeFormatter formatterForToday = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-            String[] buffChangeTime = dateTime.format(formatterForToday).split(" ");
-            String rsl = buffChangeTime[0] + " " + buffDateTime[1];
-            return LocalDateTime.parse(rsl, formatter);
+            dateTime = LocalDateTime.now();
         } else {
-            Set<Map.Entry<String, String>> calSet = calendarMap.entrySet();
-            for (Map.Entry<String, String> elem : calSet) {
-                if (buffDateTime[1].equals(elem.getValue())) {
-                    buffDateTime[1] = elem.getKey();
-                }
-            }
+            buffDateTime[1] = calendarMap.get(buffDateTime[1]);
             if (buffDateTime[0].length() == 1) {
                 String buff = buffDateTime[0];
                 buffDateTime[0] = "0" + buff;
             }
-            String rsl = "20" + buffDateTime[2].substring(0, buffDateTime[2].length() - 1)
+            rsl = "20" + buffDateTime[2].substring(0, buffDateTime[2].length() - 1)
                     + "-" + buffDateTime[1]
                     + "-" + buffDateTime[0]
                     + " "
                     + buffDateTime[3];
             return LocalDateTime.parse(rsl, formatter);
         }
+        DateTimeFormatter formatterForYesterdayAndToday = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String[] buffChangeTime = dateTime.format(formatterForYesterdayAndToday).split(" ");
+        rsl = buffChangeTime[0] + " " + buffDateTime[1];
+        return LocalDateTime.parse(rsl, formatter);
     }
 }
